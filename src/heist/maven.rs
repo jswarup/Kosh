28caac0d6eec6203b2627b823686964c358c5f77
jswarup <jswarup@users.noreply.github.com>
2026-05-26 14:23:18 +0530

    fix(heist): fix compilation errors in maven.rs

--- src/heist/maven.rs
+++ src/heist/maven.rs
@@ -1,6 +1,9 @@
 //-- maven.rs -----------------------------------------------------------------------------------------------------------------------
 
 use crate::silo::stk::Stk;
+use crate::silo::uint::{U32, U16};
+use crate::silo::stash::Stash;
+use crate::silo::atm::SpinLock;
 
 //---------------------------------------------------------------------------------------------------------------------------------
 /// Trait to abstract Atelier
@@ -15,6 +18,18 @@ trait AtelierT
     fn  FreeJobs( &mut self, stk: &mut Stk< u16>) -> u32;
     fn  IncrSzSchedJob( &mut self,  inc : u32) -> u32;
     fn  ExecuteJob( &mut self,  mavenInd : u16,  jobId : u16);
+}
+
+
+struct Maven
+{
+    _Index: U16,
+    _CurSuccId: U16,
+    _SzProcessed : U32,
+    _RunQueue : Stash< U16>,
+    _RunQlock : SpinLock,
+    _JobCache : Stash< U16>,
+    _TJobSilo : Stash< U16>,
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------
