84b7423347177daaff2d22c81f313ca9db79c2c4
jswarup <jswarup@users.noreply.github.com>
2026-06-16 12:57:23 +0530

    Move IArr to arr.rs and remove slice.rs

--- src/silo/arr.rs
+++ src/silo/arr.rs
@@ -1,10 +1,125 @@
 //-- arr.rs -----------------------------------------------------------------------------------------------------------------------
-use	crate::silo::{ U8, U32 };
+use	crate::silo::{ IAccess, U8, U32 };
+use	crate::stalks::IWorker;
 use	std::marker::PhantomData;
 use	std::ops::{ Deref, DerefMut };
 use	std::ptr::NonNull;
 
 //---------------------------------------------------------------------------------------------------------------------------------
+
+pub trait IArr< 'a, T: 'a>: IAccess< 'a, T> {
+    fn	Ptr( &self) -> *const T;
+
+    fn	MutAt< K: Into< U32>>( &self, k: K) -> &'a mut T
+    {
+        unsafe {
+            let  	ptr = self.Ptr().cast_mut().add( k.into().AsUsize());
+            &mut *ptr
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	SetAt< K: Into< U32>>( &self, k: K, a: &T) -> &'a T
+    where
+        T: Clone,
+    {
+        unsafe {
+            let  	ptr = self.Ptr().cast_mut().add( k.into().AsUsize());
+            *ptr = a.clone();
+            &*ptr
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	SwapAt< K: Into< U32>>( &self, k: K, a: &mut T) -> &'a T
+    {
+        unsafe {
+            let  	ptr = self.Ptr().cast_mut().add( k.into().AsUsize());
+            std::ptr::swap( ptr, a);
+            &*ptr
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	Swap< I: Into< U32>, J: Into< U32>>( &self, i: I, j: J)
+    {
+        unsafe {
+            std::ptr::swap( 
+                self.Ptr().cast_mut().add( i.into().AsUsize()),
+                self.Ptr().cast_mut().add( j.into().AsUsize()),
+            );
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	SwapFrom< S: Into< U32>, D: Into< U32>>( 
+        &self,
+        dstStart: D,
+        src: &Arr< '_, T>,
+        srcStart: S,
+        count: U32,
+    ) where
+        T: Copy,
+    {
+        unsafe {
+            std::ptr::swap_nonoverlapping( 
+                src.Ptr().cast_mut().add( srcStart.into().AsUsize()),
+                self.Ptr().cast_mut().add( dstStart.into().AsUsize()),
+                count.AsUsize(),
+            );
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	LSnip< C: Into< U32>>( &self, count: C) -> Arr< 'a, T>
+    {
+        let  	cnt = count.into();
+        Arr::New( 
+            unsafe { std::ptr::NonNull::new_unchecked(self.Ptr().cast_mut().add( cnt.AsU32() as usize)) },
+            self.Size() - cnt,
+        )
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	RSnip< C: Into< U32>>( &self, count: C) -> Arr< 'a, T>
+    {
+        let  	cnt = count.into();
+        Arr::New( unsafe { std::ptr::NonNull::new_unchecked(self.Ptr().cast_mut()) }, self.Size() - cnt)
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	QuickSorter< Less>( &self, less: Less) -> impl Fn( &dyn IWorker) + Send + Sync + 'a
+    where
+        Less: Fn( &T, &T) -> bool + Send + Sync + 'a + Copy,
+        T: Send + Sync + 'a,
+    {
+        let  	arr = Arr::New( unsafe { std::ptr::NonNull::new_unchecked(self.Ptr().cast_mut()) }, self.Size());
+        move |worker: &dyn IWorker| {
+            let  	lessFn = move |i, j| less( arr.At( i), arr.At( j));
+            let  	swapFn = move |i, j| arr.Swap( i, j);
+            arr.USeg().DoQSort( worker, lessFn, swapFn);
+        }
+    }
+
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	SortSanity< Less>( &self, less: Less) -> bool
+    where
+        Less: Fn( &T, &T) -> bool + Send + Sync + Clone + 'a,
+    {
+        self.USeg().RSnip( 1).Span( |k| !less( self.At( k + 1), self.At( k)))
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
 
 pub struct Arr< 'a, T>
 {
@@ -35,6 +150,28 @@ impl< 'a, T> Arr< 'a, T>
         }
     }
 
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+impl< 'a, T> IAccess< 'a, T> for Arr< 'a, T>
+{
+    fn	Size( &self) -> U32
+    {
+        self._Size
+    }
+
+    fn	At< K: Into< U32>>( &self, k: K) -> &'a T
+    {
+        unsafe { &*self.Ptr().add( k.into().AsUsize()) }
+    }
+}
+
+impl< 'a, T> IArr< 'a, T> for Arr< 'a, T> {
+    fn	Ptr( &self) -> *const T
+    {
+        self._Ptr.as_ptr()
+    }
 }
 
 //---------------------------------------------------------------------------------------------------------------------------------
