3a37cf711086eaf9a2991c9294dc9f8382e6a25e
Jyoti Swarup <jyoti.swarup@outlook.com>
2026-06-23 22:35:16 +0530

    Refactor: rename xjson.rs to jsonifc.rs

--- src/segue/mod.rs
+++ src/segue/mod.rs
@@ -4,12 +4,12 @@ pub mod instream;
 pub mod jsonoutstrm;
 pub mod outstream;
 pub mod shard;
-pub mod xjson;
+pub mod jsonifc;
 pub use	charset::Charset;
 pub use	instream::InStream;
 pub use	outstream::OutStream;
 pub use	jsonoutstrm::JsonOutStream;
-pub use	xjson::{ JsonListener, JsonValue, JSonIfc };
+pub use	jsonifc::{ JsonListener, JsonValue, JSonIfc };
 pub use	shard::Shard;
 #[cfg( test)]
 mod _tests;
