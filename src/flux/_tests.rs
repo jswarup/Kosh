e78c77dbbeb02ec13b85ca2fd25b689a8b63af65
Jyoti Swarup <jyoti.swarup@outlook.com>
2026-06-30 19:49:36 +0530

    Test: Move InStream and JsonOutStream tests to flux module

---
+++ src/flux/_tests.rs
@@ -0,0 +1,74 @@
+//-- _tests.rs ----------------------------------------------------------------------------------------------------------------------
+use	crate::{ flux::{ JsonOutStream, xflux::XField, InStream }, silo::{ U8, U32, IAccess } };
+use	std::fs;
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+struct Point 
+{
+    _X : f64,
+    _Y :f64,
+}
+
+crate::ImplIXFluxable!( Point, _X, _Y);
+
+#[test] 
+fn	TestJsonOutStream()
+{
+    let  	prices = crate::Buff![ 12.34_f32, 56.78, 90.12, 34.56, 78.90 ];
+    let  	arr = prices.Arr();
+    
+    let     pt = Point{ _X: 10.0, _Y: 30.3};
+    
+    let  	mut output = String::new();
+    {
+        let  	mut jsonStream = JsonOutStream::New( &mut output, true);
+
+        jsonStream.KeyField( "point", XField::Fluxable( &pt));
+        jsonStream.KeyField( "prices", XField::Fluxable( &arr));
+    }
+    
+    fs::write( "a.json", output).unwrap();
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+#[test]
+fn	TestInStream()
+{
+    let  	data = [U8( b'a'), U8( b'b'), U8( b'c')];
+    let  	mut stream = InStream::FromArr( (&data).into());
+    assert_eq!( stream.Curr(), U8( b'a'));
+    assert!( stream.Next());
+    assert_eq!( stream.Curr(), U8( b'b'));
+    assert!( stream.Next());
+    assert_eq!( stream.Curr(), U8( b'c'));
+    assert!( !stream.Next());
+    assert_eq!( stream.Curr(), U8::_0);
+    stream.RollTo( U32( 1));
+    assert_eq!( stream.Curr(), U8( b'b'));
+    let  	rest1 = stream.Rest();
+    assert_eq!( rest1.Size(), 2);
+    assert_eq!( *rest1.At( 0), U8( b'b'));
+    assert_eq!( *rest1.At( 1), U8( b'c'));
+    assert_eq!( stream.RemainingBytes(), b"bc");
+    stream.RollTo( U32( 5));
+    assert_eq!( stream.Curr(), U8::_0);
+    let  	rest5 = stream.Rest();
+    assert_eq!( rest5.Size(), 0);
+    assert_eq!( stream.RemainingBytes(), b"");
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+#[test]
+fn	TestInStreamFromFile()
+{
+    let  	path = "test_inbuffstream.txt";
+    fs::write( path, b"hello").unwrap();
+    let  	mut stream = InStream::FromFile( path).unwrap();
+    assert_eq!( stream.Curr(), U8( b'h'));
+    assert!( stream.Next());
+    assert_eq!( stream.Curr(), U8( b'e'));
+    fs::remove_file( path).unwrap();
+}
--- src/flux/mod.rs
+++ src/flux/mod.rs
@@ -8,6 +8,9 @@ pub mod xflux;
 pub use	instream::InStream;
 pub use	jsonoutstrm::JsonOutStream;
 pub use	outstream::OutStream;
+
+#[cfg( test)]
+mod	_tests;
 pub use	xflux::{ IXFlux, XField, IXFluxable };
 
 //---------------------------------------------------------------------------------------------------------------------------------
