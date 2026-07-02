54372bc0e2f0c9daf44a85bcacf9835648cf460b
Jyoti Swarup <jyoti.swarup@outlook.com>
2026-07-02 10:46:56 +0530

    Refactor parser: store DynINode in Forge, move match and RollTo to MatchNode

--- src/segue/parser.rs
+++ src/segue/parser.rs
@@ -1,3 +1,5 @@
+//-- parser.rs -------------------------------------------------------------------------------------------------------------------
+
 use	std::io;
 use	std::io::Read;
 use	std::cell::Cell;
@@ -21,13 +23,17 @@ pub struct  Digest
 //---------------------------------------------------------------------------------------------------------------------------------
 
 pub trait IForge<'a, 'p, 's, R: Read + 'p>
-where 's: 'p, Self: 'a
+    where 's: 'p, Self: 'a
 {
     fn	Parent( &self) -> Option< &'a (dyn IForge<'a, 'p, 's, R> + 'a)>;
     fn	Offset( &self) -> U32;
     fn	GetParser( &mut self) -> &mut Parser<'p, 's, R>;
+    fn	Node( &self) -> Option<*const DynINode<'static>>;
+    fn	SetNode( &mut self, node: Option<*const DynINode<'static>>);
     fn	Deposit( &self, childIdx: U32, digest: Digest);
     
+    //-----------------------------------------------------------------------------------------------------------------------------
+
     fn	FindAncestor( &self, predicate: &mut dyn FnMut( &(dyn IForge<'a, 'p, 's, R> + 'a)) -> bool) -> Option< &'a (dyn IForge<'a, 'p, 's, R> + 'a)>
     {
         let  	mut curr = self.Parent();
@@ -40,15 +46,22 @@ where 's: 'p, Self: 'a
         None
     }
 
-    fn	Init( &mut self) where Self: Sized
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	Init( &mut self, node: Option<*const DynINode<'static>>) where Self: Sized
     {
+        self.SetNode( node);
         let  	selfRef: &mut (dyn IForge<'a, 'p, 's, R> + 'a) = self;
         let  	ptr = selfRef as *mut (dyn IForge<'a, 'p, 's, R> + 'a);
         let  	erased_ptr = unsafe { std::mem::transmute::<_, *mut (dyn IForge<'static, 'static, 'static, R> + 'static)>(ptr) };
         self.GetParser()._Stash.Push( Some(erased_ptr));
     }
     
-    fn	Finish( &mut self) where Self: Sized {
+    //-----------------------------------------------------------------------------------------------------------------------------
+
+    fn	Finish( &mut self) 
+        where Self: Sized 
+    {
         let  	mut dummy = None;
         self.GetParser()._Stash.Pop( &mut dummy);
     }
@@ -63,14 +76,16 @@ pub trait IGrammar
 
 //---------------------------------------------------------------------------------------------------------------------------------
 pub struct  Forge<'a, 'p, 's, R: Read + 'p = io::Empty>
-where 's: 'p {
+    where 's: 'p 
+{
     pub _Parent: Option< &'a (dyn IForge<'a, 'p, 's, R> + 'a)>,
     pub _Offset: U32,
     pub _Parser: &'a mut Parser<'p, 's, R>,
+    pub _Node: Option<*const DynINode<'static>>,
 }
 
 impl<'a, 'p, 's, R: Read + 'p> IForge<'a, 'p, 's, R> for Forge<'a, 'p, 's, R>
-where 's: 'p
+    where 's: 'p
 {
     fn	Parent( &self) -> Option< &'a (dyn IForge<'a, 'p, 's, R> + 'a)>
     {
@@ -85,6 +100,16 @@ where 's: 'p
     fn	GetParser( &mut self) -> &mut Parser<'p, 's, R>
     {
         self._Parser
+    }
+
+    fn	Node( &self) -> Option<*const DynINode<'static>>
+    {
+        self._Node
+    }
+
+    fn	SetNode( &mut self, node: Option<*const DynINode<'static>>)
+    {
+        self._Node = node;
     }
 
     fn	Deposit( &self, _childIdx: U32, _digest: Digest)
@@ -120,6 +145,8 @@ where 's: 'p
     pub _Stash: Stash< Option<*mut (dyn IForge<'static, 'static, 'static, R> + 'static)>>,
 }
 
+//---------------------------------------------------------------------------------------------------------------------------------
+
 impl<'p, 's, R: Read + 'p> Parser<'p, 's, R>
 where 's: 'p
 {
@@ -131,6 +158,8 @@ where 's: 'p
         }
     }
 }
+
+//---------------------------------------------------------------------------------------------------------------------------------
 
 impl<'p, 's, R: Read + 'p> IParser<'p, 's, R> for Parser<'p, 's, R>
 where 's: 'p
@@ -236,6 +265,7 @@ where 's: 'p
     pub _Parent: Option< &'a (dyn IForge<'a, 'p, 's, R> + 'a)>,
     pub _Offset: U32,
     pub _Parser: &'a mut Parser<'p, 's, R>,
+    pub _Node: Option<*const DynINode<'static>>,
     pub _LeftDigest: Cell< Option< Digest>>,
     pub _RightDigest: Cell< Option< Digest>>,
 }
@@ -258,6 +288,16 @@ where 's: 'p
         self._Parser
     }
 
+    fn	Node( &self) -> Option<*const DynINode<'static>>
+    {
+        self._Node
+    }
+
+    fn	SetNode( &mut self, node: Option<*const DynINode<'static>>)
+    {
+        self._Node = node;
+    }
+
     fn	Deposit( &self, childIdx: U32, digest: Digest)
     {
         if childIdx == U32( 0) {
@@ -267,6 +307,8 @@ where 's: 'p
         }
     }
 }
+
+//---------------------------------------------------------------------------------------------------------------------------------
 
 impl<'a, 'p, 's, R: Read + 'p> Drop for BinOpForge<'a, 'p, 's, R>
 where 's: 'p
@@ -296,72 +338,85 @@ impl<'a> IGrammar for DynINode<'a>
                 }
                 false
             }
-            ChildOp::Less => {
+            ChildOp::Less | ChildOp::Bor => {
                 let  	mut forge = BinOpForge {
                     _Parent: parent,
                     _Offset: startMark,
                     _Parser: parser,
+                    _Node: None,
                     _LeftDigest: Cell::new( None),
                     _RightDigest: Cell::new( None),
                 };
-                forge.Init();
+                let  	node_ptr = unsafe { std::mem::transmute::<*const DynINode<'a>, *const DynINode<'static>>(self as *const DynINode<'a>) };
+                forge.Init( Some(node_ptr));
+                return forge.MatchNode();
+            }
+            _ => {
+                false
+            }
+        }
+    }
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------
+
+impl<'a, 'p, 's, R: Read + 'p> BinOpForge<'a, 'p, 's, R>
+where 's: 'p
+{
+    pub fn MatchNode( &mut self) -> bool
+    {
+        let  	node = unsafe { &*(self._Node.unwrap() as *const DynINode<'_>) };
+        
+        match node.ChildOp() {
+            ChildOp::Less => {
+                let  	leftChild = node._At( U32( 0));
+                let  	rightChild = node._At( U32( 1));
                 
-                let  	leftChild = self._At( U32( 0));
-                let  	rightChild = self._At( U32( 1));
-                
-                if leftChild.Match( forge.GetParser()) {
-                    if rightChild.Match( forge.GetParser()) {
-                        let  	endMark = forge.GetParser().Stream().Marker();
-                        let  	digest = Digest { _Start: startMark, _End: endMark };
-                        if let Some( p) = forge.Parent() {
+                if leftChild.Match( self.GetParser()) {
+                    if rightChild.Match( self.GetParser()) {
+                        let  	endMark = self.GetParser().Stream().Marker();
+                        let  	digest = Digest { _Start: self._Offset, _End: endMark };
+                        if let Some( p) = self.Parent() {
                             p.Deposit( U32( 0), digest);
                         }
                         return true;
                     }
                 }
                 
-                forge.GetParser().Stream().RollTo( startMark);
+                let  	startMark = self._Offset;
+                self.GetParser().Stream().RollTo( startMark);
                 false
             }
             ChildOp::Bor => {
-                let  	mut forge = BinOpForge {
-                    _Parent: parent,
-                    _Offset: startMark,
-                    _Parser: parser,
-                    _LeftDigest: Cell::new( None),
-                    _RightDigest: Cell::new( None),
-                };
-                forge.Init();
+                let  	leftChild = node._At( U32( 0));
+                let  	rightChild = node._At( U32( 1));
                 
-                let  	leftChild = self._At( U32( 0));
-                let  	rightChild = self._At( U32( 1));
-                
-                if leftChild.Match( forge.GetParser()) {
-                    let  	endMark = forge.GetParser().Stream().Marker();
-                    let  	digest = Digest { _Start: startMark, _End: endMark };
-                    if let Some( p) = forge.Parent() {
+                if leftChild.Match( self.GetParser()) {
+                    let  	endMark = self.GetParser().Stream().Marker();
+                    let  	digest = Digest { _Start: self._Offset, _End: endMark };
+                    if let Some( p) = self.Parent() {
                         p.Deposit( U32( 0), digest);
                     }
                     return true;
                 }
                 
-                forge.GetParser().Stream().RollTo( startMark);
+                let  	startMark = self._Offset;
+                self.GetParser().Stream().RollTo( startMark);
                 
-                if rightChild.Match( forge.GetParser()) {
-                    let  	endMark = forge.GetParser().Stream().Marker();
-                    let  	digest = Digest { _Start: startMark, _End: endMark };
-                    if let Some( p) = forge.Parent() {
+                if rightChild.Match( self.GetParser()) {
+                    let  	endMark = self.GetParser().Stream().Marker();
+                    let  	digest = Digest { _Start: self._Offset, _End: endMark };
+                    if let Some( p) = self.Parent() {
                         p.Deposit( U32( 0), digest);
                     }
                     return true;
                 }
                 
-                forge.GetParser().Stream().RollTo( startMark);
+                let  	startMark = self._Offset;
+                self.GetParser().Stream().RollTo( startMark);
                 false
             }
-            _ => {
-                false
-            }
+            _ => false
         }
     }
 }
@@ -376,3 +431,5 @@ impl<'a, 'r> IGrammar for &'r DynINode<'a>
         return res;
     }
 }
+
+//---------------------------------------------------------------------------------------------------------------------------------
