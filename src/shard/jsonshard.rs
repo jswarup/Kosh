0e010d5a80112063f41a2cefdd5d3844db063704
Jyoti Swarup <jyoti.swarup@outlook.com>
2026-07-19 19:11:35 +0530

    refactor: merge MatchValue into IGrammar::Match for JsonShard

--- src/shard/jsonshard.rs
+++ src/shard/jsonshard.rs
@@ -5,7 +5,6 @@ use	crate::{
     ShardTree,
 
     shard::{ IGrammar, Parser, WSpc },
-    silo::{ U32, U8},
 };
 use	crate::shard::numbers::Real;
 
@@ -16,73 +15,23 @@ pub const Json: JsonShard = JsonShard;
 
 //---------------------------------------------------------------------------------------------------------------------------------
 
-
-
 impl IGrammar for JsonShard
 {
     fn	Match( &self, parser: &mut Parser) -> bool
     {
-        let  	res = JsonShard::MatchValue( parser);
-        if let Some( newM) = res {
+        let  	m = parser.CurrMark(); 
+        let  	objShard = ShardTree!( '{' < *(*WSpc < Str < *WSpc < ':' < *WSpc < ( |p: &mut Parser| p.ParseGrammar( &Json, p.CurrMark()).is_some()) < ? ( ',' < *WSpc)) < *WSpc < '}');
+        let  	arrShard = ShardTree!( '[' < *(*WSpc < ( |p: &mut Parser| p.ParseGrammar( &Json, p.CurrMark()).is_some()) < ? ( ',' < *WSpc)) < *WSpc < ']');
+        let  	keyShard = ShardTree!( Str | "true" | "false" | "null" | Real );
+        let  	valShard = ShardTree!( *WSpc < ( keyShard | arrShard | objShard) );
+        
+        if let Some( newM) = parser.ParseGrammar( &valShard, m) {
             let  	nextM = if let Some( m2) = parser.ParseGrammar( &WSpc, newM) { m2 } else { newM };
             parser.SetCurrMark( nextM);
             true
         } else {
             false
         }
-    }
-}
-
-//---------------------------------------------------------------------------------------------------------------------------------
-
-impl JsonShard
-{
-
-
-    fn	MatchValue<'a>( parser: &mut Parser) -> Option< U32>
-    {
-        let  	mut m = parser.CurrMark();
-        if let Some( newM) = parser.ParseGrammar( &WSpc, m) {
-            m = newM;
-            parser.SetCurrMark( m);
-        }
-
-        let  	curr = parser.GetAt( m);
-        if curr == U8( b'{') {
-            return Self::MatchObject( parser);
-        } 
-        if curr == U8( b'[') {
-            return Self::MatchArray( parser);
-        }
-        let  	shardTree = ShardTree!( Str | "true" | "false" | "null" | Real );
-        if let Some( newM) = parser.ParseGrammar( &shardTree, m) {
-            return Some( newM);
-        }  
-        None
-    }
-
-    //---------------------------------------------------------------------------------------------------------------------------------
-
-    fn	MatchArray<'a>( parser: &mut Parser) -> Option< U32>
-    {
-        let  	m = parser.CurrMark();
-        let  	shardTree = ShardTree!( '[' < *(*WSpc < ( |p: &mut Parser| Self::MatchValue( p).is_some()) < ? ( ',' < *WSpc)) < *WSpc < ']');
-        if let Some( newM) = parser.ParseGrammar( &shardTree, m) {
-            return Some( newM);
-        }  
-        None 
-    }
-
-    //---------------------------------------------------------------------------------------------------------------------------------
-
-    fn	MatchObject<'a>( parser: &mut Parser) -> Option< U32>
-    {
-        let  	m = parser.CurrMark();
-        let  	shardTree = ShardTree!( '{' < *(*WSpc < Str < *WSpc < ':' < *WSpc < ( |p: &mut Parser| Self::MatchValue( p).is_some()) < ? ( ',' < *WSpc)) < *WSpc < '}');
-        if let Some( newM) = parser.ParseGrammar( &shardTree, m) {
-            return Some( newM);
-        }  
-        None 
     }
 }
 
